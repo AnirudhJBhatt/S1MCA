37a6838fd805fdf69039c26f92d7b4b4bac85b59
AnirudhJBhatt <148520535+AnirudhJBhatt@users.noreply.github.com>
2023-12-28 10:47:00 +0530

    Add files via upload

---
+++ Web Programming Lab/CO2/questions.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+        question: 'What is the capital of France?',
+        answers: [
+            { text: 'Paris', correct: true },
+            { text: 'Berlin', correct: false },
+            { text: 'Madrid', correct: false },
+            { text: 'Rome', correct: false }
+        ]
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        answers: [
+            { text: 'Mars', correct: true },
+            { text: 'Venus', correct: false },
+            { text: 'Jupiter', correct: false },
+            { text: 'Saturn', correct: false }
+        ]
+    },
+    // Add more questions as needed
+];
